a7f093a657ec9fd5a66460654af6d73414363f74
N33MO <neemo.cdy@gmail.com>
2020-02-13 09:36:47 -0600

    VR update

---
+++ Assignment 2/Asg2-3DTravel/Assets/Scripts/GunShoot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunShoot : MonoBehaviour
+{
+    public GameObject projectile;
+    public AudioClip shootSound;
+
+    private float bulletSpeed = 50f;
+    private AudioSource source;
+    private float volLoRange = 0.5f;
+    private float volHiRange = 1.0f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            float vol = Random.Range(volLoRange, volHiRange);
+            source.PlayOneShot(shootSound, vol);
+            GameObject throwThis = Instantiate (projectile, transform.position, transform.rotation) as GameObject;
+            throwThis.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, bulletSpeed));
+        }
+
+    }
+}
---
+++ VR-coursework/Source.md
@@ -0,0 +1,3 @@
+1.wood material: https://assetstore.unity.com/packages/2d/textures-materials/wood/wooden-floor-materials-150564
+
+2.
